2f571fd88a1b86d788245a342e02a9042c665bea
2211cs010142 <167754854+2211cs010142@users.noreply.github.com>
2025-02-06 10:35:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Welcome from './welcomeelcome';
+
+// const Welcome=()=><h1 className='message'>Hello Charan</h1>
+
+// function App(){
+//   return(
+//     <div className='App'>
+//         <Welcome/>  
+//     </div>
+//   )
+// }
+
+const App=()=>{
+  return(
+    <div style={{textAlign:'center',color:'blue'}}>
+      <Welcome greeting="Good morning" name="Charan"/>
+      <Welcome greeting="Hii" name="Murali"/>
+    </div>
+  )
+}
+export default App;
+
+
